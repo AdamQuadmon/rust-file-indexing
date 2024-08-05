c40132702228458326e70286f94401c85bf6a39e
NielsBongers <nielsbongers5@gmail.com>
2024-08-05 16:19:23 +0200

    Rayon working nicely

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,9 @@ use std::io::Read;
 use std::path::{Path, PathBuf};
 use std::time::SystemTime;
 
+use env_logger::{Builder, Env};
+use log::{error, info, warn};
+
 use rust_folder_analysis::path_data::PathData;
 use std::io::{Error, ErrorKind};
 
@@ -15,6 +18,9 @@ use std::sync::{Arc, Mutex};
 
 use std::fs::read_dir;
 
+/// Converts Option<&OsStr> to Option<String>.
+/// We can't save a reference in a struct so we need to do this instead.
+/// If any conversions fail, this will return a None.
 fn path_os_str_to_string(path_os_str: Option<&std::ffi::OsStr>) -> Option<String> {
     match path_os_str {
         Some(path) => match path.to_str() {
@@ -28,10 +34,11 @@ fn path_os_str_to_string(path_os_str: Option<&std::ffi::OsStr>) -> Option<String
 fn construct_entry(dir_entry: &DirEntry) -> Result<PathData, Error> {
     let path = dir_entry.path();
 
+    // PathBuf to save in the struct.
     let path_buf = path.to_path_buf();
 
     // This works for both files and folders.
-    // If it fails, something is really wrong.
+    // If it fails, something is really wrong, so this will return an Error.
     let name = path
         .file_name()
         .ok_or_else(|| Error::new(ErrorKind::NotFound, "Failed to access path name"))?
@@ -62,9 +69,12 @@ fn construct_entry(dir_entry: &DirEntry) -> Result<PathData, Error> {
         Err(_) => (None, None, None),
     };
 
+    // Extension from OsStr to String.
     let extension = path_os_str_to_string(path.extension());
+
     let is_folder = path.is_dir();
 
+    // Creating a result.
     Ok(PathData::new(
         path_buf, name, stem, size, extension, created, modified, is_folder,
     ))
@@ -82,43 +92,78 @@ fn build_index(
             for path in folder_contents {
                 match path {
                     Ok(dir_entry) => {
+                        // Turning everything into a struct based on the entry.
                         let index_entry_result = construct_entry(&dir_entry);
 
                         if let Ok(index_entry) = index_entry_result {
-                            // println!("Index entry: {:?}\n", index_entry);
+                            let entry_path = index_entry.path.to_owned();
 
-                            let entry_path = index_entry.path.clone();
-
+                            // We need to save to two separate places so this clone is necessary if we have a folder.
                             if index_entry.is_folder {
                                 folder_queue.push(entry_path);
                             }
 
+                            // Saving to the index reference vector.
                             path_results.push(index_entry);
                         }
                     }
                     Err(e) => {
-                        println!("Error: {:?}", e);
+                        error!("Failed to read path entry: {:?}", e);
                     }
                 }
             }
         }
-        Err(_) => {
-            println!("Failed to read folder.")
+        Err(e) => {
+            error!("Failed to read folder {:?}: {}.", folder_path, e)
         }
     }
 }
 
 fn main() {
-    let trial_path = Path::new(r"D:\Downloads\OneDrive_2024-08-05\CORE Drive");
+    Builder::from_env(Env::default().default_filter_or("info")).init();
 
-    let mut folder_queue = Vec::<PathBuf>::new();
-    let mut path_results = Vec::<PathData>::new();
+    let trial_path = Path::new(r"D:\");
 
-    loop {
-        build_index(trial_path, &mut folder_queue, &mut path_results);
+    let folder_queue = Arc::new(Mutex::new(vec![trial_path.to_path_buf()]));
+    let path_results = Arc::new(Mutex::new(Vec::<PathData>::new()));
 
-        if folder_queue.len() == 0 {
-            break;
+    let pool = rayon::ThreadPoolBuilder::new()
+        .num_threads(20)
+        .build()
+        .unwrap();
+
+    pool.scope(|s| {
+        loop {
+            let folders = {
+                let mut queue = folder_queue.lock().unwrap();
+                if queue.is_empty() {
+                    break;
+                }
+                // Extract the folders to process in this iteration
+                queue.drain(..).collect::<Vec<_>>()
+            };
+
+            // Process folders in parallel
+            folders.into_par_iter().for_each(|folder_path| {
+                let mut new_folders = Vec::new();
+                let mut results = Vec::new();
+
+                build_index(&folder_path, &mut new_folders, &mut results);
+
+                // Safely update the shared folder_queue and path_results
+                {
+                    let mut queue = folder_queue.lock().unwrap();
+                    queue.extend(new_folders);
+                }
+                {
+                    let mut results_guard = path_results.lock().unwrap();
+                    results_guard.extend(results);
+                }
+            });
         }
-    }
+    });
+
+    let results = Arc::try_unwrap(path_results).unwrap().into_inner().unwrap();
+
+    info!("Results: {}", results.len());
 }
